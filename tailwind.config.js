05f927ecc979aab6a3ea23981e1c00fafa37d4c0
Daniel <lamthevinh003@gmail.com>
2024-11-22 15:14:55 +0700

    landing page option 3

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+
+        screens: {
+          xl: "1248px",
+        },
+      },
+      colors: {
+        "dark-blue": "rgba(23, 44, 55, 1)",
+        "sub-dark-blue": "rgba(23, 32, 57, 1)",
+        "primary-dark-blue": "rgba(31, 42, 68, 1)",
+        "sub-orange": "rgba(178, 144, 99, 1)",
+        "bg-sub-orange": "rgba(215, 196, 169, 0.9)",
+      },
+    },
+  },
+  plugins: [],
+};
